40962aba7ae3440e3dda906878b925e67adf4d12
Luis Ribeiro <luis.ribeiro@stud.fh-campuswien.ac.at>
2020-11-25 02:19:33 +0100

    * ClientTest

--- client/src/main/java/at/falb/games/alcatraz/api/ClientRun.java
+++ client/src/main/java/at/falb/games/alcatraz/api/ClientRun.java
@@ -2,7 +2,6 @@ package at.falb.games.alcatraz.api;
 
 import at.falb.games.alcatraz.api.logic.ClientValues;
 import at.falb.games.alcatraz.api.utilities.ClientCfg;
-import at.falb.games.alcatraz.api.utilities.CommonValues;
 import at.falb.games.alcatraz.api.utilities.JsonHandler;
 import at.falb.games.alcatraz.api.utilities.ServerCfg;
 import org.apache.commons.lang3.StringUtils;
@@ -30,6 +29,51 @@ public class ClientRun {
             clientCfg.setStartTimestamp(START_TIMESTAMP);
             ServerCfg serverCfg = JsonHandler.readServerJson(serverName);
             LocateRegistry.getRegistry(serverCfg.getServerIp(), serverCfg.getRegistryPort());
+
+            List<ServerInterface> serverList = new ArrayList<>();
+
+            // hier probiert der Client die Servers nacheinander zu lokalisieren,
+            // falls ein Server down ist, wird die Exception ignoriert und mit dem nächsten Server probiert.
+            Registry Reg = LocateRegistry.getRegistry(serverCfg.getServerIp(), serverCfg.getRegistryPort());
+            try {
+                serverList.add((ServerInterface) Reg.lookup(serverCfg.getName()));
+            } catch (Exception ignore) {
+            }
+            serverCfg = JsonHandler.readServerJson("secondary");
+            Reg = LocateRegistry.getRegistry(serverCfg.getServerIp(), serverCfg.getRegistryPort());
+            try {
+                serverList.add((ServerInterface) Reg.lookup(serverCfg.getName()));
+            } catch (Exception ignore) {
+            }
+            serverCfg = JsonHandler.readServerJson("tertiary");
+            Reg = LocateRegistry.getRegistry(serverCfg.getServerIp(), serverCfg.getRegistryPort());
+            try {
+                serverList.add((ServerInterface) Reg.lookup(serverCfg.getName()));
+            } catch (Exception e) {
+                System.out.println("Cannot locate any server!!");
+            }
+            GamePlayer player1 = new GamePlayer(0);
+            player1.setName(args[0]);
+            int ID = 0;
+            System.out.println("Press 1 for register or 0 for deregister:");
+            Scanner in = new Scanner(System.in);
+            int x = in.nextInt();
+            if (x == 1) {
+                try {
+                    for (ServerInterface SI : serverList) {
+                        ID = SI.register(player1);
+                    }
+                } catch (SpreadException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Player ID: " + ID);
+
+            } else if (x == 0) {
+                for (ServerInterface SI : serverList) {
+                    SI.deregister(player1);
+                }
+            }
+
         } catch (Exception e) {
             LOG.error("It wasn't possible to start the client", e);
         }
@@ -37,59 +81,10 @@ public class ClientRun {
 
         //////////////////////////// Testing purposes
 
-        List<ServerInterface> serverList = new ArrayList<ServerInterface>();
 
-        // hier probiert der Client die Servers nacheinander zu lokalisieren,
-        // falls ein Server down ist, wird die Exception ignoriert und mit dem nächsten Server probiert.
-        Registry Reg = LocateRegistry.getRegistry(serverCfg.getServerIp(), serverCfg.getRegistryPort());
-        try {serverList.add((ServerInterface) Reg.lookup(serverCfg.getName()));} catch (Exception ignore) { }
-            serverCfg = JsonHandler.readServerJson("secondary");
-            Reg = LocateRegistry.getRegistry(serverCfg.getServerIp(), serverCfg.getRegistryPort());
-        try {serverList.add((ServerInterface) Reg.lookup(serverCfg.getName()));} catch (Exception ignore) { }
-            serverCfg = JsonHandler.readServerJson("tertiary");
-            Reg = LocateRegistry.getRegistry(serverCfg.getServerIp(), serverCfg.getRegistryPort());
-        try {
-            serverList.add((ServerInterface) Reg.lookup(serverCfg.getName()));
-        } catch (Exception e) {
-            System.out.println("Cannot locate any server!!" );
-        }
-        GamePlayer player1 = new GamePlayer(0);
-        player1.setName(args[0]);
-        int ID = 0;
-        System.out.println("Press 1 for register or 0 for deregister:");
-        Scanner in = new Scanner(System.in);
-        int x = in.nextInt();
-        if (x == 1) {
-            try {
-                for (ServerInterface SI : serverList) {
-                    ID = SI.register(player1);
-                    if (ID != CommonValues.SERVER_NOT_PRIMARY) break;
-                }
-            } catch (SpreadException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Player ID: " + ID);
-
-        }
-        else if (x == 0){
-            try {
-                for (ServerInterface SI : serverList){
-                    ID = SI.deregister(player1);
-                    if (ID != CommonValues.SERVER_NOT_PRIMARY) break;
-                }
-            } catch (SpreadException e) {
-                e.printStackTrace();
-            }
-        }
 
         ////////////////////////////////////////// End of testing
 
-// TODO to be removed
-//        ServerInterface service = (ServerInterface) Reg.lookup(ClientValues.MAIN_SERVER);
-//        ClientInterface client_1 = new Client(clientCfg.getIp() , clientCfg.getPort());
-//        Registry Reg_1 = LocateRegistry.createRegistry(client_1.getPlayer().getPort());
-//        Reg_1.rebind("", client_1);
-//        client_1.getPlayer().setName(clientCfg.getName());
 
     }
 
