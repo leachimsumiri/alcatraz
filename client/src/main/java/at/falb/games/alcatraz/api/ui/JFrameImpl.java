37dcbe8cade0f7a48ddebd9f5198fa073b0f0880
BajicK <katharina.bajic@ait.ac.at>
2020-11-29 12:42:30 +0100

    adapted ui to work with server

--- client/src/main/java/at/falb/games/alcatraz/api/ui/JFrameImpl.java
+++ client/src/main/java/at/falb/games/alcatraz/api/ui/JFrameImpl.java
@@ -7,7 +7,7 @@ import at.falb.games.alcatraz.api.exceptions.BeginGameException;
 import at.falb.games.alcatraz.api.exceptions.GamePlayerException;
 import at.falb.games.alcatraz.api.logic.Client;
 import at.falb.games.alcatraz.api.logic.InputHelper;
-import at.falb.games.alcatraz.api.utilities.ServerClientUtility;
+import at.falb.games.alcatraz.api.utilities.*;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -31,12 +31,18 @@ public class JFrameImpl {
     }
 
     public static void main(String[] args) throws RemoteException {
+        try {
+            String serverName = args.length == 2 && StringUtils.isNotBlank(args[0]) ? args[0] : ClientValues.MAIN_SERVER;
+            final ServerCfg firstServer = JsonHandler.readServerJson(serverName);
+        } catch (IOException e) {
+            LOG.error("Something went wrong", e);
+        }
 
         ClientInterface client = new Client();
 
         // https://javatutorial.net/swing-jframe-basics-create-jframe
         JFrame frame= new JFrame(gc);
-        frame.setTitle("Welecome to JavaTutorial.net");
+        frame.setTitle("Welecome to the Alcatraz Lobby");
         frame.setSize(600, 400);
         frame.setLocation(200, 200);
 
@@ -49,9 +55,9 @@ public class JFrameImpl {
         frame.getContentPane().add(textfield1);
 
         // https://javatutorial.net/jframe-buttons-listeners-text-fields
-        JButton b = new JButton("Submit");
-        b.setBounds(100,100,140, 40);
-        frame.add(b);
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBounds(100,100,140, 40);
+        frame.add(submitButton);
 
         JButton startGameButton = new JButton("Start Game");
         startGameButton.setBounds(100,100,140, 40);
@@ -67,22 +73,27 @@ public class JFrameImpl {
         label1.setBounds(10, 110, 200, 100);
         frame.add(label1);
 
-        String[] data = {"Player 1", "Player 2", "Player 3", "Player 4"};
-        frame.add(new JList(data));
+        JTextArea txtPlayerList = new JTextArea("");
+        txtPlayerList.setEditable(false);
+        Font font = new Font("Verdana", Font.PLAIN, 15);
+        txtPlayerList.setFont(font);
+        frame.add(txtPlayerList);
 
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
 
+        UpdatePlayerThread thread = new UpdatePlayerThread(client, frame, txtPlayerList);
+
         //action listener
-        b.addActionListener(new ActionListener() {
+        submitButton.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent arg0) {
+                // Submit name
                 String name;
                 GamePlayer gamePlayer = null;
                 try {
-                    //TODO: logic - working?
                     gamePlayer = InputHelper.getInstance().requestPlayerSocket();
                     client.setGamePlayer(gamePlayer);
                     ServerClientUtility.createRegistry(client);
@@ -93,16 +104,18 @@ public class JFrameImpl {
                     gamePlayer.setName(name);
                     int id = client.getPrimary().register(gamePlayer);
                     gamePlayer.setId(id);
+                    LOG.info("Player registered: " + name);
+                    thread.start();
 
                     // layout
                     textfield1.setText(name);
                     textfield1.setEnabled(false);
-                    b.setEnabled(false);
+                    submitButton.setEnabled(false);
                     startGameButton.setEnabled(true);
                     deregisterButton.setEnabled(true);
-
                 } catch (IOException | SpreadException | GamePlayerException | NotBoundException e) {
-                    e.printStackTrace();
+                    LOG.error("Something went wrong", e);
+                    JOptionPane.showMessageDialog(frame, e.getMessage());
                 }
             }
         });
@@ -111,18 +124,18 @@ public class JFrameImpl {
 
             @Override
             public void actionPerformed(ActionEvent arg0) {
-
-                //TODO: logic start game - working?
+                // Start game
                 try {
                     client.getPrimary().beginGame();
-                } catch (RemoteException | SpreadException | BeginGameException | NotBoundException | MalformedURLException e) {
-                    e.printStackTrace();
+                    frame.setVisible(false);
+                    frame.dispose();
+                } catch (BeginGameException e) {
+                    LOG.error(e.getMessage());
+                    JOptionPane.showMessageDialog(frame, e.getMessage());
+                }catch (RemoteException | SpreadException | NotBoundException | MalformedURLException e) {
+                    LOG.error("Something went wrong", e);
+                    JOptionPane.showMessageDialog(frame, e.getMessage());
                 }
-
-                //close Jframe window
-                frame.setVisible(false);
-                frame.dispose();
-
             }
         });
 
@@ -130,21 +143,19 @@ public class JFrameImpl {
 
             @Override
             public void actionPerformed(ActionEvent arg0) {
-
-                // TODO: logic
-                /*try {
+                // Deregister name
+                try {
+                    GamePlayer gamePlayer = client.getGamePlayer();
                     client.getPrimary().deregister(gamePlayer);
+                    LOG.info("Deregistered player: " + gamePlayer.getName());
                 } catch (SpreadException | RemoteException | GamePlayerException | NotBoundException | MalformedURLException e) {
-                    e.printStackTrace();
-                }*/
-
-                // graphics
-                textfield1.setText(null);
-                textfield1.setEnabled(true);
-                b.setEnabled(true);
-                startGameButton.setEnabled(false);
-                deregisterButton.setEnabled(false);
-
+                    LOG.error("Something went wrong", e);
+                    JOptionPane.showMessageDialog(frame, e.getMessage());
+                }
+                thread.setStopThread(true);
+                frame.setVisible(false);
+                frame.dispose();
+                System.exit(0);
             }
         });
     }
--- client/src/main/java/at/falb/games/alcatraz/api/utilities/UpdatePlayerThread.java
+++ client/src/main/java/at/falb/games/alcatraz/api/utilities/UpdatePlayerThread.java
@@ -6,6 +6,8 @@ import at.falb.games.alcatraz.api.logic.InputHelper;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import javax.swing.*;
+import java.awt.*;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,26 +17,46 @@ public class UpdatePlayerThread extends Thread {
     private static final Logger LOG = LogManager.getLogger(UpdatePlayerThread.class);
 
     ClientInterface client;
+    JFrame frame;
+    JTextArea textField;
+    Boolean stopThread = false;
 
-    public UpdatePlayerThread(ClientInterface client) {
+    public UpdatePlayerThread(ClientInterface client, JFrame frame, JTextArea textField) {
         this.client = client;
+        this.frame = frame;
+        this.textField = textField;
+    }
+
+    public void setStopThread(Boolean stopThread) {
+        this.stopThread = stopThread;
     }
 
     public void run() {
         List<GamePlayer> lastGamePlayersList = new ArrayList<>();
         boolean toExit = false;
+
         do {
             try {
                 toExit = client.getPrimary().getGameStatus().equals(GameStatus.STARTED);
                 List<GamePlayer> currentGamePlayersList = client.getGamePlayersList();
+
                 if (!lastGamePlayersList.equals(currentGamePlayersList)) {
-                    InputHelper.getInstance().printPlayerList(currentGamePlayersList);
+                    StringBuilder playerList = new StringBuilder();
+                    playerList.append("Current registered players:\n");
+                    int counter = 1;
+                    for (GamePlayer gamePlayer : currentGamePlayersList) {
+                        playerList.append("Player ").append(counter).append(": ").append(gamePlayer.getName()).append('\n');
+                        counter++;
+                    }
+                    this.textField.setText(playerList.toString());
+                    frame.setVisible(true);
                     lastGamePlayersList = currentGamePlayersList;
                 }
                 TimeUnit.MILLISECONDS.sleep(500);
             } catch (RemoteException | InterruptedException e) {
                 LOG.error("Primary not available", e);
             }
-        } while(!toExit);
+        } while(!toExit && !stopThread);
+
     }
 }
